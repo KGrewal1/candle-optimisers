8371af34f9719e4edf7988297c98b09a21be40d6
Kirpal Grewal <kirpal@outlook.com>
2023-11-17 15:56:18 +0000

    add lbfgs test

--- src/lbfgs.rs
+++ src/lbfgs.rs
@@ -12,9 +12,11 @@ pub enum LineSearch {
     StrongWolfe,
 }
 
-/// LBFGS optimiser
+/// LBFGS optimiser: see Nocedal
 ///
 /// Described in <https://link.springer.com/article/10.1007/BF01589116>]
+///
+/// https://sagecal.sourceforge.net/pytorch/index.html
 
 #[derive(Debug)]
 pub struct ParamsLBFGS {
@@ -63,9 +65,12 @@ impl<M: Model> LossOptimizer<M> for Lbfgs<M> {
 
     fn backward_step(&mut self, xs: &Tensor, ys: &Tensor) -> CResult<()> {
         let loss = self.model.loss(xs, ys)?;
+
         let grads = loss.backward()?;
+
         // let mut evals = 1;
         let mut q = flatten_grads(self.vars.clone(), &grads)?;
+
         let yk = if let Some(ref last) = self.last_grad {
             last.set(&q)?;
             (&q - last.as_tensor())?
@@ -74,26 +79,47 @@ impl<M: Model> LossOptimizer<M> for Lbfgs<M> {
             q.clone()
         };
         let hist_size = self.hist.len();
+        println!("hist_size {}", hist_size);
         let gamma = if let Some((s, y)) = self.hist.back() {
             let numr = (y * s)?.sum_all()?;
-            let denom = y.sqr()?.sum_all()?;
-            (numr / denom)?.to_scalar::<f64>()?
+            let denom = &y.sqr()?.sum_all()?;
+            (numr / denom)?
+                .to_dtype(candle_core::DType::F64)?
+                .to_scalar::<f64>()?
         } else {
             self.learning_rate()
         };
+
         let mut rhos = Vec::with_capacity(hist_size);
         let mut alphas = Vec::with_capacity(hist_size);
         for (s, y) in &self.hist {
-            let rho = (y * s)?.sum_all()?.powf(-1.)?;
-            let alpha = (&rho * (s * &q)?.sum_all()?)?;
-            q = q.sub(&(y * &alpha)?)?;
+            let rho = (y * s)?
+                .sum_all()?
+                .to_dtype(candle_core::DType::F64)?
+                .to_scalar::<f64>()?
+                .powi(-1);
+
+            let alpha = &rho
+                * (s * &q)?
+                    .sum_all()?
+                    .to_dtype(candle_core::DType::F64)?
+                    .to_scalar::<f64>()?;
+
+            q = q.sub(&(y * alpha)?)?;
+
             alphas.push(alpha);
             rhos.push(rho);
         }
+
         q = (q * gamma)?;
+
         for (((s, y), alpha), rho) in self.hist.iter().zip(alphas).zip(rhos) {
-            let beta = rho * (y * &q)?.sum_all()?;
-            q = q.add(&(s * (alpha - beta)?)?)?;
+            let beta = rho
+                * (y * &q)?
+                    .sum_all()?
+                    .to_dtype(candle_core::DType::F64)?
+                    .to_scalar::<f64>()?;
+            q = q.add(&(s * (alpha - beta))?)?;
         }
         add_grad(&mut self.vars, &q)?;
 
@@ -102,7 +128,7 @@ impl<M: Model> LossOptimizer<M> for Lbfgs<M> {
         }
         self.hist.push_back((q, yk));
 
-        todo!()
+        Ok(())
     }
 
     fn learning_rate(&self) -> f64 {
@@ -120,7 +146,7 @@ impl<M: Model> LossOptimizer<M> for Lbfgs<M> {
 }
 
 fn flatten_grads(vs: Vec<Var>, grads: &GradStore) -> CResult<Tensor> {
-    let mut flat_grads = Vec::new();
+    let mut flat_grads = Vec::with_capacity(vs.len());
     for v in vs {
         if let Some(grad) = grads.get(&v) {
             flat_grads.push(grad.flatten_all()?);
@@ -145,16 +171,54 @@ fn add_grad(vs: &mut Vec<Var>, flat_tensor: &Tensor) -> CResult<()> {
     Ok(())
 }
 
-// fn unflatten(vs: Vec<Var>, flat_tensor: Tensor) -> CResult<Vec<Tensor>> {
-//     let mut tensors = Vec::new();
-//     let mut offset = 0;
-//     for var in vs {
-//         let n_elems = var.elem_count();
-//         let tensor = flat_tensor
-//             .narrow(0, offset, n_elems)?
-//             .reshape(var.shape())?;
-//         tensors.push(tensor);
-//         offset += n_elems;
-//     }
-//     Ok(tensors)
-// }
+#[cfg(test)]
+mod tests {
+    // use candle_core::test_utils::{to_vec0_round, to_vec2_round};
+
+    use crate::Model;
+    use anyhow::Result;
+    use assert_approx_eq::assert_approx_eq;
+    use candle_core::{DType, Device};
+    use candle_core::{Module, Result as CResult};
+    use candle_nn::{VarBuilder, VarMap};
+
+    use super::*;
+    #[test]
+    fn lr_test() -> Result<()> {
+        let params = ParamsLBFGS {
+            lr: 0.004,
+            ..Default::default()
+        };
+        // Now use backprop to run a linear regression between samples and get the coefficients back.
+        pub struct LinearModel {
+            linear: candle_nn::Linear,
+        }
+
+        impl Model for LinearModel {
+            fn new(vs: VarBuilder) -> CResult<Self> {
+                let linear = candle_nn::linear(2, 1, vs.pp("ln1"))?;
+                Ok(Self { linear })
+            }
+
+            fn forward(&self, xs: &Tensor) -> CResult<Tensor> {
+                self.linear.forward(xs)
+            }
+            fn loss(&self, xs: &Tensor, ys: &Tensor) -> CResult<Tensor> {
+                let preds = self.forward(xs)?;
+                let loss = candle_nn::loss::mse(&preds, ys)?;
+                Ok(loss)
+            }
+        }
+
+        // create a new variable store
+        let varmap = VarMap::new();
+        // create a new variable builder
+        let vs = VarBuilder::from_varmap(&varmap, DType::F32, &Device::Cpu);
+        let model = LinearModel::new(vs)?;
+        let mut lbfgs = Lbfgs::new(varmap.all_vars(), params, model)?;
+        assert_approx_eq!(0.004, lbfgs.learning_rate());
+        lbfgs.set_learning_rate(0.002);
+        assert_approx_eq!(0.002, lbfgs.learning_rate());
+        Ok(())
+    }
+}
---
+++ tests/lbfgs_tests.rs
@@ -0,0 +1,93 @@
+#[cfg(feature = "mkl")]
+extern crate intel_mkl_src;
+
+#[cfg(feature = "accelerate")]
+extern crate accelerate_src;
+
+// use candle_core::test_utils::{to_vec0_round, to_vec2_round};
+
+use anyhow::Result;
+use candle_core::{DType, Result as CResult};
+use candle_core::{Device, Tensor};
+use candle_nn::{Linear, Module, VarBuilder, VarMap};
+use optimisers::lbfgs::{Lbfgs, ParamsLBFGS};
+use optimisers::{LossOptimizer, Model};
+
+/* The results of this test have been checked against the following PyTorch code.
+    import torch
+    from torch import optim
+
+    w_gen = torch.tensor([[3., 1.]])
+    b_gen = torch.tensor([-2.])
+
+    sample_xs = torch.tensor([[2., 1.], [7., 4.], [-4., 12.], [5., 8.]])
+    sample_ys = sample_xs.matmul(w_gen.t()) + b_gen
+
+    m = torch.nn.Linear(2, 1)
+    with torch.no_grad():
+        m.weight.zero_()
+        m.bias.zero_()
+    optimiser = optim.Adamax(m.parameters(), lr=0.004)
+    # optimiser.zero_grad()
+    for _step in range(100):
+        optimiser.zero_grad()
+        ys = m(sample_xs)
+        loss = ((ys - sample_ys)**2).sum()
+        loss.backward()
+        optimiser.step()
+        # print("Optimizer state begin")
+        # print(optimiser.state)
+        # print("Optimizer state end")
+    print(m.weight)
+    print(m.bias)
+*/
+#[test]
+fn lbfgs_test() -> Result<()> {
+    // Generate some linear data, y = 3.x1 + x2 - 2.
+    let w_gen = Tensor::new(&[[3f32, 1.]], &Device::Cpu)?;
+    let b_gen = Tensor::new(-2f32, &Device::Cpu)?;
+    let gen = Linear::new(w_gen, Some(b_gen));
+    let sample_xs = Tensor::new(&[[2f32, 1.], [7., 4.], [-4., 12.], [5., 8.]], &Device::Cpu)?;
+    let sample_ys = gen.forward(&sample_xs)?;
+
+    pub struct LinearModel {
+        linear: candle_nn::Linear,
+    }
+
+    impl Model for LinearModel {
+        fn new(vs: VarBuilder) -> CResult<Self> {
+            let linear = candle_nn::linear(2, 1, vs.pp("ln1"))?;
+            Ok(Self { linear })
+        }
+
+        fn forward(&self, xs: &Tensor) -> CResult<Tensor> {
+            self.linear.forward(xs)
+        }
+        fn loss(&self, xs: &Tensor, ys: &Tensor) -> CResult<Tensor> {
+            let preds = self.forward(xs)?;
+            let loss = candle_nn::loss::mse(&preds, ys)?;
+            Ok(loss)
+        }
+    }
+
+    let params = ParamsLBFGS {
+        lr: 0.004,
+        ..Default::default()
+    };
+
+    // create a new variable store
+    let varmap = VarMap::new();
+    // create a new variable builder
+    let vs = VarBuilder::from_varmap(&varmap, DType::F32, &Device::Cpu);
+    let model = LinearModel::new(vs)?;
+    let mut lbfgs = Lbfgs::new(varmap.all_vars(), params, model)?;
+
+    for _step in 0..5000 {
+        println!("start step {}", _step);
+        lbfgs.backward_step(&sample_xs, &sample_ys)?;
+        println!("end step {}", _step);
+    }
+    panic!("stop");
+
+    Ok(())
+}
